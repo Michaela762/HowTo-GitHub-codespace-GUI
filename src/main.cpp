1d622713ae9af1c012337da16c2f5ad9bc5ce64d
Michaela762 <misa.chalova@gmail.com>
2026-04-02 10:08:56 +0000

    změna

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,71 @@
 #include <iostream>
-#include <string>
+#include <cmath>
 
 int main()
 {
     std::cout << "Vitejte v kalkulacce" << std::endl;
-    std::cout << "Zadejte prvni cislo: ";
-    float a;
-    std::cin >> a;
-    std::cout << "Zadejte druhe cislo: ";
-    float b;
-    std::cin >> b;
+    std::string continueAnswer = "ano";
 
-    float sum = a + b;
-    float difference = a - b;
-    float product = a * b;
-    float quotient = a / b;
+    while (continueAnswer == "ano")
+    {
+        std::cout << "Zadejte prvni cislo:" << std::endl;
+        float a;
+        std::cin >> a;
 
-    std::cout << "Soucet: " << sum << std::endl;
-    std::cout << "Rozdil: " << difference << std::endl;
-    std::cout << "Soucin: " << product << std::endl;
-    std::cout << "Podil: " << quotient << std::endl;
-    std::cout << "Dekuji za pouziti kalkulacky, aplikaci ukoncite libovolnou klavesou." << std::endl;
-    /*  std::cin.get();
-     std::cin.get(); */
+        std::cout << "Zadejte druhe cislo:" << std::endl;
+        float b;
+        std::cin >> b;
+
+        std::cout << "Zvolte si operaci:" << std::endl;
+        std::cout << "1 - scitani" << std::endl;
+        std::cout << "2 - odcitani" << std::endl;
+        std::cout << "3 - nasobeni" << std::endl;
+        std::cout << "4 - deleni" << std::endl;
+
+        int choice;
+        std::cin >> choice;
+
+        float result = 0.0f;
+        bool choiceValid = true;
+
+        switch (choice)
+        {
+        case 1:
+            result = a + b;
+            break;
+
+        case 2:
+            result = a - b;
+            break;
+
+        case 3:
+            result = a * b;
+            break;
+
+        case 4:
+            result = a / b;
+            break;
+
+        default:
+            // neplatna volba
+            choiceValid = false;
+            break;
+        }
+
+        if (choiceValid)
+        {
+            std::cout << "Vysledek: " << result << std::endl;
+        }
+        else
+        {
+            std::cout << "Neplatna volba" << std::endl;
+        }
+
+        std::cout << "Prejete si zadat dalsi priklad? [ano/ne]" << std::endl;
+        std::cin >> continueAnswer;
+    }
+
+    std::cout << "Dekuji za pouziti kalkulacky." << std::endl;
 
     return 0;
 }
